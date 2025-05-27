caefa941aa38949b75d47bff72f82bfb928ebc85
Thenexus96 <nsanford96@gmail.com>
2025-05-26 21:58:47 -0700

    Initial commit. Creation

---
+++ src/main/java/net/sanfonic/hivemind/HivemindTemplateModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sanfonic.hivemind;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HivemindTemplateModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
