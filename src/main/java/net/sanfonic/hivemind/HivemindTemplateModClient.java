3490da451d997657e82cee3e5ce176f4c2cd9403
Thenexus96 <nsanford96@gmail.com>
2025-05-26 22:27:53 -0700

    Updated Mod Detail's

---
+++ src/main/java/net/sanfonic/hivemind/HivemindTemplateModClient.java
@@ -0,0 +1,10 @@
+package net.sanfonic.hivemind;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HivemindTemplateModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
