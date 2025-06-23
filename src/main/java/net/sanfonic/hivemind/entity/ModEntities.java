20cfeddb792ac7d8393d17c93760c782456a9d5f
Thenexus96 <nsanford96@gmail.com>
2025-06-23 01:09:44 -0700

    Created HiveMindAccessItem and HiveMindData Updated ModEntities and ModItems to reflect progress towards getting the mod setup for controls. Changed Texture from Iron Golem to Dark Steve Skin. Also added Things-to-change.md for a reminder to change hive_core.json reference texture Made fixes recommended by Claude

--- src/main/java/net/sanfonic/hivemind/entity/ModEntities.java
+++ src/main/java/net/sanfonic/hivemind/entity/ModEntities.java
@@ -1,19 +1,22 @@
 package net.sanfonic.hivemind.entity;
 
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
+
 import net.sanfonic.hivemind.HivemindTemplateMod;
 import net.sanfonic.hivemind.golems.CombatGolem;
 
 public class ModEntities {
-    public static final EntityType<CombatGolem> COMBAT_GOLEM = Registry.register(Registries.ENTITY_TYPE,
-            new Identifier("hivemind", "combat_golem"),
-            FabricEntityTypeBuilder.create(SpawnGroup.MONSTER, CombatGolem::new)
+    public static final EntityType<CombatGolem> COMBAT_GOLEM = Registry.register(
+            Registries.ENTITY_TYPE,
+            new Identifier(HivemindTemplateMod.MOD_ID, "combat_golem"),  //FIXED: Use main MOD_ID
+            FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, CombatGolem::new) //FIXED: Changed from MONSTER to CREATURE
             .dimensions(EntityDimensions.fixed(0.75f, 1.75f))
             .build()
     );
--- src/main/java/net/sanfonic/hivemind/item/ModItems.java
+++ src/main/java/net/sanfonic/hivemind/item/ModItems.java
@@ -1,15 +1,23 @@
 package net.sanfonic.hivemind.item;
 
+import net.sanfonic.hivemind.HivemindTemplateMod;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 
 public class ModItems {
+    public static final Item HIVE_MIND_ACCESS = registerItem("hive_mind_access",
+            new HiveMindAccessItem(new FabricItemSettings().maxCount(1).rarity(Rarity.RARE)));
+
     public static final Item HIVE_CORE = new Item(new Item.Settings());
 
     public static void register() {
-        Registry.register(Registries.ITEM, new Identifier("hivemind", "hive_core"), HIVE_CORE);
-        System.out.println("ModItems Registered!");
+        //FIXED: Use Consistent MOD_ID
+        Registry.register(Registries.ITEM, new Identifier(HivemindTemplateMod.MOD_ID, "hive_core"), HIVE_CORE);
+        HivemindTemplateMod.LOGGER.info("ModItems Registered for" + HivemindTemplateMod.MOD_ID);
     }
 }
